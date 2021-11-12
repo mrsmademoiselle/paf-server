0a04f4ace1abc798dde2cc5dc0889b93de5f6246
chris <Chrs.kaiser3@live.de>
2021-11-12 12:40:49 +0100

    initial push

---
+++ memorio/src/main/java/com/memorio/memorio/MemorioApplication.java
@@ -0,0 +1,13 @@
+package com.memorio.memorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorioApplication.class, args);
+	}
+
+}
---
+++ memorio/src/test/java/com/memorio/memorio/MemorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memorio.memorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
