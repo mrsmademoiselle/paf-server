bae6c55931dc51bbe2dd850776342570246631c0
chris <Chrs.kaiser3@live.de>
2021-11-29 12:22:04 +0100

    implements cross origin annotation

--- src/main/java/com/memorio/memorio/web/controller/UserController.java
+++ src/main/java/com/memorio/memorio/web/controller/UserController.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties.MatchingStrategy;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +28,7 @@ import java.util.List;
  übernimmt JPA bestimmte Operationen automatisch, z.B. das committen von Changes (Persistence Context) */
 @Transactional
 // localhost:9090/user
+@CrossOrigin("http://localhost:9090")
 @RequestMapping("/user")
 public class UserController {
     Logger logger = LoggerFactory.getLogger(UserController.class);
