b4b5d639aaf8b54a7e99569f941b93b1ae5260ec
purrito <franziska.loof@stud.th-luebeck.de>
2021-12-31 17:54:54 +0100

    flipCard erweitert

--- src/main/java/com/memorio/memorio/entities/Board.java
+++ src/main/java/com/memorio/memorio/entities/Board.java
@@ -9,6 +9,7 @@ import javax.persistence.Embeddable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Das Board repräsentiert das fertige Spielbrett im Spiel.
@@ -39,4 +40,45 @@ public class Board {
         return cardset;
     }
 
+    /**
+     * Ist dafür zuständig, den Flipstatus einer Karte beim Umdrehen zu setzen.
+     * Dafür wird geprüft, ob die Karte mit anderen, umgedrehten Karten matcht.
+     */
+    public void flipCard(String cardId) {
+        List<Card> cardSet = getCardSet();
+        System.out.println("karten davor: " + cardSet.toString());
+
+        // hole die Karte, die geflippt werden soll
+        Card currentCard = cardSet.stream()
+                .filter(card -> card.getId().equals(cardId))
+                .findFirst().orElse(null);
+
+        if (currentCard == null) return;
+
+        // hole alle Karten, die bereits vom User in dieser Runde aufgedeckt wurden
+        List<Card> allCardsWaitingToBeFlipped = cardSet.stream()
+                .filter(card -> card.getFlipStatus().equals(FlipStatus.WAITING_TO_FLIP))
+                .collect(Collectors.toList());
+
+        // Wenn es eine Karte gibt die auf das flippen wartet und sie mit unserer Karte matcht, flippe beide
+        if (allCardsWaitingToBeFlipped.size() == 1) {
+            Card cardWaitingToBeFlipped = allCardsWaitingToBeFlipped.get(0);
+            if (cardWaitingToBeFlipped.getPairId() == currentCard.getPairId()) {
+                currentCard.flipCard();
+                cardWaitingToBeFlipped.flipCard();
+            } else { // ansonsten unflippe beide
+                currentCard.unflipCard();
+                cardWaitingToBeFlipped.unflipCard();
+            }
+            // Wenn es keine Karten gibt die auf das flippen warten, bringe currentCard in den Wartemodus bis zum nächsten Kartenzug
+        } else if (allCardsWaitingToBeFlipped.size() == 0) {
+            currentCard.waitToFlip();
+            // wenn mehr als 1 Karte wartet (darf eigentlich nicht passieren),
+            // dann ist was schiefgegangen und wir unflippen alle erstmal
+        } else {
+            allCardsWaitingToBeFlipped.forEach(Card::unflipCard);
+        }
+
+        System.out.println("karten danach: " + getCardSet().toString());
+    }
 }
--- src/main/java/com/memorio/memorio/services/GameHandler.java
+++ src/main/java/com/memorio/memorio/services/GameHandler.java
@@ -1,11 +1,6 @@
 package com.memorio.memorio.services;
 
-import com.memorio.memorio.entities.Card;
-import com.memorio.memorio.entities.FlipStatus;
 import com.memorio.memorio.entities.Game;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Dient der Persistierung und Verarbeitung eines einzigen Game-Objekts zur Laufzeit.
@@ -24,39 +19,7 @@ public class GameHandler {
     }
 
     public void flipCard(String cardId) {
-        List<Card> cardSet = gameInstance.getBoard().getCardSet();
-
-        // hole die Karte, die geflippt werden soll
-        Card currentCard = cardSet.stream()
-                .filter(card -> card.getId().equals(cardId))
-                .findFirst().orElse(null);
-
-        if (currentCard == null) return;
-
-        // hole alle Karten, die bereits vom User in dieser Runde aufgedeckt wurden
-        List<Card> allCardsWaitingToBeFlipped = cardSet.stream()
-                .filter(card -> card.getFlipStatus().equals(FlipStatus.WAITING_TO_FLIP))
-                .collect(Collectors.toList());
-
-        // Wenn es eine Karte gibt die auf das flippen wartet und sie mit unserer Karte matcht, flippe beide
-        if (allCardsWaitingToBeFlipped.size() == 1) {
-            Card cardWaitingToBeFlipped = allCardsWaitingToBeFlipped.get(0);
-            if (cardWaitingToBeFlipped.getPairId() == currentCard.getPairId()) {
-                currentCard.flipCard();
-                cardWaitingToBeFlipped.flipCard();
-            } else { // ansonsten unflippe beide
-                currentCard.unflipCard();
-                cardWaitingToBeFlipped.unflipCard();
-            }
-            // Wenn es keine Karten gibt die auf das flippen warten, bringe currentCard in den Wartemodus bis zum nächsten Kartenzug
-        } else if (allCardsWaitingToBeFlipped.size() == 0) {
-            currentCard.waitToFlip();
-            // wenn mehr als 1 Karte wartet (darf eigentlich nicht passieren),
-            // dann ist was schiefgegangen und wir unflippen alle erstmal
-        } else {
-            allCardsWaitingToBeFlipped.forEach(Card::unflipCard);
-        }
-
+        gameInstance.getBoard().flipCard(cardId);
     }
 
     public Game getGame() {
